578df73708d5b50ed9bb10778d39c7f58187a620
biosjuan <hsoftwaredeveloperlee@gmail.com>
2023-08-31 09:06:22 -0700

    fix home

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import React from 'react';
+import Link from 'next/link';
 import { cookies } from 'next/headers';
 import CountCard from './tasks/_components/CountCard';
 
@@ -6,68 +7,90 @@ async function getDashboardData() {
   try {
     const cookieStore = cookies();
     const token = cookieStore.get('token')?.value;
-    const endPont = `${process.env.domain}/api/dashboard`;
-    const response = await fetch(endPont, {
+    const endPoint = `${process.env.domain}/api/dashboard`;
+    const response = await fetch(endPoint, {
       cache: 'no-cache',
-      headers: { Cookie: `token=${token}` },
+      headers: {
+        Cookie: `token=${token}`,
+      },
     });
     const { data } = await response.json();
     console.log(data);
     return data;
-  } catch (error: any) {
+  } catch (error) {
     return [];
   }
 }
-
 async function Home() {
   const dashboardData = await getDashboardData();
   return (
     <div>
       <h1 className='text-2xl font-semibold text-gray-600'>
-        Welcome to Next-Task Manager
+        Welcome To Next-Task Manager
       </h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 gap-5'>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 gap-10'>
         <CountCard
-          title={'All Tasks'}
+          title='All Tasks'
           count={dashboardData.totalTasks}
           path='/tasks'
-          queryParams={{ status: '' }}
+          queryParams={{
+            status: '',
+          }}
         />
+
         <CountCard
-          title={'Open Tasks'}
+          title='Open Tasks'
           count={dashboardData.openTasks}
           path='/tasks'
-          queryParams={{ status: 'open' }}
+          queryParams={{
+            status: 'open',
+          }}
         />
+
         <CountCard
-          title={'Completed Tasks'}
+          title='Completed Tasks'
           count={dashboardData.completedTasks}
           path='/tasks'
-          queryParams={{ status: 'completed' }}
+          queryParams={{
+            status: 'completed',
+          }}
         />
+
         <CountCard
-          title={'In Progress Tasks'}
+          title='In Progress Tasks'
           count={dashboardData.inProgressTasks}
           path='/tasks'
-          queryParams={{ status: 'in-progress' }}
+          queryParams={{
+            status: 'in-progress',
+          }}
         />
+
         <CountCard
-          title={'High Priority Tasks'}
+          title='High Priority Tasks'
           count={dashboardData.highPriorityTasks}
           path='/tasks'
-          queryParams={{ priority: 'high' }}
+          queryParams={{
+            priority: 'high',
+          }}
         />
+
         <CountCard
-          title={'Medium Priority Tasks'}
+          title='Medium Priority Tasks'
           count={dashboardData.mediumPriorityTasks}
           path='/tasks'
-          queryParams={{ priority: 'medium' }}
+          queryParams={{
+            priority: 'medium',
+          }}
         />
+
         <CountCard
-          title={'Low Priority Tasks'}
+          title='Low Priority Tasks'
           count={dashboardData.lowPriorityTasks}
           path='/tasks'
-          queryParams={{ priority: 'low' }}
+          queryParams={{
+            priority: 'low',
+          }}
         />
       </div>
     </div>
